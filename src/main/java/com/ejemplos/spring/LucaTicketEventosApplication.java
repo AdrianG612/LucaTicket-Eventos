50045c34b434efb4f444fea947e536e7dcadc1cf
AdrianG612 <adrian.gomez@formacion.luca-tic.com>
2024-12-10 16:12:40 +0100

    carcasa dev

--- src/main/java/com/ejemplos/spring/LucaTicketEventosApplication.java
+++ src/main/java/com/ejemplos/spring/LucaTicketEventosApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LucaTicketEventosApplication {
 
+	//hola
 	public static void main(String[] args) {
 		SpringApplication.run(LucaTicketEventosApplication.class, args);
 	}
